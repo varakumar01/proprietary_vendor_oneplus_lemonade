940c49a9222ed96a1800b62e214376f3440c7a70
Jammy555 <prathap.venkata@gmail.com>
2026-02-11 18:33:06 +0530

    lemonade: Remove firmware
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,31 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),lemonade)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,4875db77f095947337b42d6e48f7a7510f56f4ed)
-$(call add-radio-file-sha1-checked,radio/aop.img,3e033686a647dd2fe23fbc39af9d4c98409c4290)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,38e647dd23be4568c07469f6921a97bfab16a39a)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,52cb0bcb8480f8856f4ef0a8813ce8c0cc4a5bdb)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,4e1da0901e98eaf31eee90f6b51639aa8972b222)
-$(call add-radio-file-sha1-checked,radio/dsp.img,634718247c9670c0c2aab842804ea74c3f274396)
-$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,84ecec2070fc2a23aaaf471f3eff08cedf8bfd62)
-$(call add-radio-file-sha1-checked,radio/hyp.img,148eeedbf5e60f8985d16eb4ca8f7381bc55d5b6)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,2586fe40b79420a79b482018e71217b65a81cac0)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,6e3c015df630cb557568c63e7e225059a8412b34)
-$(call add-radio-file-sha1-checked,radio/modem.img,06158b1c72a8f6dd33241516c5d9ae35b57ee49e)
-$(call add-radio-file-sha1-checked,radio/multiimgoem.img,ad753dac4e4ed7edb2270eeb7ed7e02bc3417c07)
-$(call add-radio-file-sha1-checked,radio/oplus_sec.img,70c1fb07ea5d63af49361d57b2a5f5d22a7b5612)
-$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,850c9ff65ecf49be365ccd5c4a593a5b6462e2b8)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,7aa42a838baf74668240c809813036d38653727d)
-$(call add-radio-file-sha1-checked,radio/qweslicstore.img,180cf2903b9872499d05eeef08c472575fa3225e)
-$(call add-radio-file-sha1-checked,radio/shrm.img,0adc328819a8617e0c7551ddbe9bca6b1df881d7)
-$(call add-radio-file-sha1-checked,radio/splash.img,cef1d4886175660f305e78ab39b7782f8625e431)
-$(call add-radio-file-sha1-checked,radio/tz.img,bc2f9470824081f12d5e85c6604e0e7ef171f279)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,50ba113a5eb388b089e6d320dadf37a06a9405e0)
-$(call add-radio-file-sha1-checked,radio/xbl.img,37f8991c99d572aa450ddadfb13915e24d37e7b9)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,49ab8d83f097669d6d75b3e4b0d04cedefa29a9d)
-
-endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -3,26 +3,4 @@
 #
 
 AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    bluetooth \
-    cpucp \
-    devcfg \
-    dsp \
-    engineering_cdt \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    multiimgoem \
-    oplus_sec \
-    oplusstanvbk \
-    qupfw \
-    qweslicstore \
-    shrm \
-    splash \
-    tz \
-    uefisecapp \
-    xbl \
-    xbl_config
+ 
